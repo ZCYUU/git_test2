97e8238060ada50244ba19b3e99daa9e360b6cb5
zzcc <1320312557@qq.com>
2022-06-07 21:35:23 +0800

    second commit67

--- src/main/java/com/atguigu/git/GT2.java
+++ src/main/java/com/atguigu/git/GT2.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GT2 {
     public static void main(String[] args) {
         System.out.println("hello.git,,,");
+        System.out.println("hello git 222");
     }
 }
