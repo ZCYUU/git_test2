89f432ee91b2af51ec90395387b9e8113e49f2fc
zzcc <1320312557@qq.com>
2022-06-07 21:57:26 +0800

    hot-fix1 commit67

--- src/main/java/com/atguigu/git/GT2.java
+++ src/main/java/com/atguigu/git/GT2.java
@@ -4,5 +4,6 @@ public class GT2 {
     public static void main(String[] args) {
         System.out.println("hello.git,,,");
         System.out.println("hello git 222");
+        System.out.println("hot-fix update1");
     }
 }
